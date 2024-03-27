66c6f892f1edbec22b2c8925c3853cf437178c59
hackboxguy <albert.david@gmail.com>
2024-03-27 12:40:15 +0100

    added file health check

--- sd-wire-load-image.sh
+++ sd-wire-load-image.sh
@@ -11,23 +11,53 @@ if [ ! -f "$1" ]; then
 	exit 1
 fi
 
-#check if the given file is .tar.xz/.gz/.zip compressed
+#check if the given file is .tar.xz/.gz/.zip compressed and healthy
 IMG_COMPRESSION="none"
 FILEMSG=$(file $1)
 echo $FILEMSG | grep "XZ compressed data" > /dev/null
-[ $? = 0 ] && IMG_COMPRESSION="xz"
+if [ $? = 0 ]; then
+	echo -n "Checking xz Health  : "
+	xz -t $1 1>/dev/null 2>/dev/null
+	if [ $? = 0 ]; then
+		echo "ok"
+	else
+		echo "error! exiting"
+		exit 1
+	fi
+	IMG_COMPRESSION="xz"
+fi
 
 echo $FILEMSG | grep "gzip compressed data" > /dev/null
-[ $? = 0 ] && IMG_COMPRESSION="gz"
+if [ $? = 0 ]; then
+	echo -n "Checking gz Health  : "
+	gzip -t $1 1>/dev/null 2>/dev/null
+	if [ $? = 0 ]; then
+		echo "ok"
+	else
+		echo "error! exiting"
+		exit 1
+	fi
+	IMG_COMPRESSION="gz"
+fi
 
 echo $FILEMSG | grep "Zip archive data" > /dev/null
-[ $? = 0 ] && IMG_COMPRESSION="zip"
+if [ $? = 0 ]; then
+	echo -n "Checking zip Health : "
+	unzip -t $1 1>/dev/null 2>/dev/null
+	if [ $? = 0 ]; then
+		echo "ok"
+	else
+		echo "error! exiting"
+		exit 1
+	fi
+	IMG_COMPRESSION="zip"
+fi
 
 if [ $IMG_COMPRESSION = "none" ]; then
         echo "Unsupported file type! only .xz/.gz/.zip are supported types"
         exit 1
 fi
-echo "Detected Image      : $IMG_COMPRESSION"
+#echo "Detected Image      : $IMG_COMPRESSION"
 
 #detect if SDWire hw is connected
 SDSERIAL=$(sd-mux-ctrl --list |tail -1 | awk '{print $6}' | sed 's/,//')
@@ -39,12 +69,14 @@ else
 fi
 
 #switch the sdcard from target-device to our test-server
+service fstab stop
 usb-port-power.sh off
 sleep $SLEEPTIME
 usb-port-power.sh on
 sleep 5
 sd-mux-ctrl --device-serial=$SDSERIAL --ts
 sleep 5
+service fstab boot
 echo "Recycle USB port pwr: ok"
 
 #check if /dev/sda or /dev/sdb shows up
@@ -85,6 +117,7 @@ else
 fi
 sync
 sleep 5	
+
 echo -n "Writing img to disk : "
 #uncompress file directly to /dev/sdX node
 if [ $IMG_COMPRESSION = "xz" ]; then
@@ -92,7 +125,8 @@ if [ $IMG_COMPRESSION = "xz" ]; then
 elif [ $IMG_COMPRESSION = "gz" ]; then
 	zcat $1 > $SDWIRENODE
 elif [ $IMG_COMPRESSION = "zip" ]; then
-	gzip -dc $1 > $SDWIRENODE
+	#gzip -dc $1 > $SDWIRENODE
+	unzip -p $1 > $SDWIRENODE
 fi
 sync
 echo "done"
